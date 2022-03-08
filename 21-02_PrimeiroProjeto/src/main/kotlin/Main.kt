14ece31a0184ef9bd73c578ada0a9e27cde24402
Daniel Feliciano <dani.felic7@gmail.com>
2022-03-08 12:17:09 -0300

    Primeiro Projeto

---
+++ 21-02_PrimeiroProjeto/src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main() {
+    println("Cornélios - Bom dia Júnior!")
+    println("Júnior - Bom dia seu Cornélios!")
+    println("Cornélios - Tudo certo?")
+    println("Júnior - Tudo certo e com o senhor?")
+    println("Cornélios - Mais ou menos. Você viu o jogo?")
+    println("Júnior - Vish, eu vi seu Cornélios!")
+    println("Cornélios - Como pode 15 pênaltis meu irmão? 15 pênaltis?!")
+    println("Júnior - Hahahahahah pois é meu inacreditável.")
+    println("Cornélios - Vitinho Júnior! Vitinho!")
+    println("Júnior - Sim cara, eu pensei a mesma coisa, " +
+            "como coloca o Vitinho pra bater aquele pênalti?")
+    println("Cornélios - Quem é Vitinho?! Pelo amor de Deus!")
+    println("Júnior - Pra quê dar essa responsabilidade pra um moleque?")
+    println("Cornélios - Exatamente, aquele técnico tem que cair." +
+            " É inadmissível uma coisa dessas.")
+    println("Júnior - Ah ele vai cair. Segunda já anunciam, certeza." +
+            " E se não anunciarem a Urubuzada vai invadir o CT.")
+    println("Cornélios - E eu sou o primeiro!")
+}
